4d5e20c99f2775b790ca521fabf3ec75271afd91
uchuhimo <uchuhimo@outlook.com>
2021-01-18 00:27:51 +0800

    record xml/json/txt

--- index.js
+++ index.js
@@ -1,169 +1,178 @@
-const {KeepLiveTCP} = require('bilibili-live-ws');
-const EventEmitter = require('events');
+const { KeepLiveTCP } = require('bilibili-live-ws');
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const config = require('./config');
 let currentLive = new Map();
 
 app.get('/api/live', function (req, res) {
     const roomId = parseInt(req.query.roomID);
     const status = parseInt(req.query.status);
-    const filename = req.query.filename;
-    if (roomId && filename) {
-        console.log(roomId + ': ' + status);
-        watch({roomId, status, filename});
-        res.send({'msg': 1});
+    const path = req.query.filename;
+    if (status == 1) {
+        console.log(path);
+        currentLive[roomId] = new LiveEvent(roomId, path);
     } else {
-        res.send({'msg': 0})
+        currentLive[roomId].newPath = path;
+        currentLive[roomId].live.close();
+        delete currentLive[roomId];
     }
 });
-app.listen(config.ExpressPort, function () {
-    console.log(`listening on port ${config.ExpressPort}!`);
-});
-const watch = ({roomId, status, filename}) => {
-    if (!currentLive[roomId] && status === 1) {
-        new LiveEvent(roomId, filename).emit('start');
+app.listen(3000);
+
+
+class DanmaMessage {
+    constructor({
+        cmd,
+        uid,
+        username,
+        message,
+        guard,
+        is_admin,
+        type,
+        fontSize,
+        color,
+        timestamp
+    }) {
+        this.cmd = cmd;
+        this.uid = uid;
+        this.username = username;
+        this.message = message;
+        this.guard = guard;
+        this.is_admin = is_admin;
+        this.type = type;
+        this.fontSize = fontSize;
+        this.color = color;
+        this.timestamp = timestamp;
     }
-    currentLive[roomId] = status;
-};
+}
 
-class LiveEvent extends EventEmitter {
-    constructor(roomId, filename) {
-        super();
+function parseMessage(message) {
+    switch (message["cmd"]) {
+        case 'DANMU_MSG':
+            const info = message["info"];
+            return new DanmaMessage({
+                cmd: message["cmd"],
+                uid: info[2][0],
+                username: info[2][1],
+                message: info[1],
+                guard: info[7],
+                is_admin: info[2][2] === 1,
+                type: info[0][1],
+                fontSize: info[0][2],
+                color: info[0][3],
+                timestamp: info[0][4],
+            })
+        case 'SEND_GIFT':
+        case 'GUARD_BUY':
+        case 'USER_TOAST_MSG':
+        case 'SUPER_CHAT_MESSAGE':
+        case 'SUPER_CHAT_MESSAGE_JPN':
+        case 'WELCOME':
+        case 'WELCOME_GUARD':
+            return { cmd: message["cmd"], ...message["data"] };
+        default:
+            return { cmd: message["cmd"] };
+    }
+}
 
+class LiveEvent {
+    constructor(roomId, path) {
         this.roomId = roomId;
-        this.live = new KeepLiveTCP(roomId);
+        this.path = path;
+        this.txtFile = fs.openSync(path + ".txt", 'a+');
+        this.xmlFile = fs.openSync(path + ".xml", 'a+');
+        this.jsonFile = fs.openSync(path + ".json", 'a+');
+        this.afterFirst = false;
         this.startTime = Date.now();
 
-        this.count = 0;
-        this.prevTime = "";
-        this.prevText = "";
-
-        this.path = `${config.DownloadDir}/${filename}`;
-        this.txtFile = fs.createWriteStream(this.path, {flags: 'a+'});
-        if (config.SRT || config.VTT) this.srtFile = fs.createWriteStream(this.path.split('.txt')[0] + '.srt', {flags: 'a+'});
-        this.watch();
-        this.on('start', () => {
-            this.monitor(this.roomId);
+        const live = new KeepLiveTCP(roomId);
+        this.live = live;
+        live.on('open', () => {
+            fs.writeSync(this.xmlFile, `<?xml version="1.0" encoding="UTF-8"?>
+<i>
+<chatserver>chat.bilibili.com</chatserver>
+<chatid>0</chatid>
+<mission>0</mission>
+<maxlimit>2147483647</maxlimit>
+<state>0</state>
+<app>vtb_record</app>
+<roomid>${roomId}</roomid>
+<source>n-a</source>
+`);
+            fs.writeSync(this.jsonFile, '[\n');
+            console.log(`${roomId} Connection is established`);
         });
-        this.on('close', () => {
-            this.live.close();
-            if (config.SRT || config.VTT) {
-                this.writeSubtitle(null);
-                this.writeVTT();
-            }
-            this.writePlainText(null);
+        live.on('live', () => {
+            live.on('msg', (data) => {
+                let message = parseMessage(data);
+                this.writeRaw(data);
+                this.writePlainText(message);
+                this.writeDanmu(message);
+            })
+        })
+        live.on('close', () => {
+            fs.writeSync(this.xmlFile, '</i>');
+            fs.writeSync(this.jsonFile, '\n]');
+            fs.closeSync(this.txtFile);
+            fs.closeSync(this.xmlFile);
+            fs.closeSync(this.jsonFile);
+            let newPath = this.newPath;
+            fs.renameSync(path + ".txt", newPath + ".txt");
+            fs.renameSync(path + ".xml", newPath + ".xml");
+            fs.renameSync(path + ".json", newPath + ".json");
+            console.log(`${roomId} Connection is closed`);
         })
     }
 
-    writeSubtitle(text) {
-        let lastTime = Date.now() - this.startTime;
-        let h = Math.floor(lastTime / 1000 / 60 / 60);
-        let m = Math.floor(lastTime / 1000 / 60 % 60);
-        let s = Math.floor(lastTime / 1000 % 60);
-        let ms = Math.floor(lastTime % 1000);
-
-        let t1 = (time) => ((time < 10) ? '0' : '') + time;
-        let t2 = (time) => ((time < 10) ? '00' : (time < 100) ? '0' : '') + time;
-
-        let now = `${t1(h)}:${t1(m)}:${t1(s)}.${t2(ms)}`;
-
-        if (this.count !== 0) {
-            try {
-                this.srtFile.write(`${this.count}\n${this.prevTime} --> ${now}\n${this.prevText}\n`);
-            } catch (e) {
-                //pass
-            }
-
-        }
-        this.count++;
-
-        if (!text) {
-            this.srtFile.close();
+    writeDanmu(message) {
+        if (message.cmd != 'DANMU_MSG') {
             return;
         }
-        this.prevTime = now;
-        this.prevText = text;
+        const lastTime = Date.now() - this.startTime;
+        const second = Math.floor(lastTime / 1000);
+        const damnu = message.message.replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&apos;');
+        fs.writeSync(this.xmlFile, `<d p="${second},${message.type},${message.fontSize},${message.color},${message.timestamp},0,${message.uid},0">${damnu}</d>\n`)
     }
 
-    writePlainText(s) {
-        if (!s) {
-            this.txtFile.close();
-            return
+    writePlainText(message) {
+        switch (message.cmd) {
+            case 'DANMU_MSG':
+                fs.writeSync(this.txtFile, `${message.username}: ${message.message}\n`);
+                return;
+            case 'SEND_GIFT':
+                fs.writeSync(this.txtFile, `${message.uname} ${message.action} ${message.num} 个 ${message.giftName}\n`);
+                return;
+            case 'GUARD_BUY':
+            case 'USER_TOAST_MSG':
+                fs.writeSync(this.txtFile, `欢迎 ${message.uname} 上舰：${message.num} 个 ${message.gift_name}\n`);
+                return;
+            case 'SUPER_CHAT_MESSAGE':
+                fs.writeSync(this.txtFile, `${message.user_info.uname} 的 ${message.price} 元SC：${message.message}\n`);
+                return;
+            case 'SUPER_CHAT_MESSAGE_JPN':
+                fs.writeSync(this.txtFile, `${message.user_info.uname} 的 ${message.price} 元SC：${message.message_jpn}\n`);
+                return;
+            case 'WELCOME':
+                fs.writeSync(this.txtFile, `欢迎 ${message.uname} 进入直播间\n`);
+                return;
+            case 'WELCOME_GUARD':
+                fs.writeSync(this.txtFile, `欢迎 ${message.uname} 老爷进入直播间\n`);
+                return;
+            default:
+                return;
         }
-        try {
-            this.txtFile.write(s + "\n")
-        } catch (e) {
-            //pass
+    }
+
+    writeRaw(data) {
+        if (this.afterFirst) {
+            fs.writeSync(this.jsonFile, ',\n');
+        } else {
+            this.afterFirst = true;
         }
-
-    }
-
-    srt2VTT(srt) {
-        srt = srt.replace(/\r+/g, '');
-        srt = srt.replace(/^\s+|\s+$/g, '');
-        let cuelist = srt.split('\n');
-        let result = "";
-        if (cuelist.length > 0) {
-            result += "WEBVTT\n";
-            for (let i = 0; i < cuelist.length; i += 3) {
-                // result += cuelist[i] + "\n";
-                result += cuelist[i + 1] + "\n";
-                result += `- ${cuelist[i + 2]}\n`
-            }
-        }
-        return result;
-    }
-
-    async writeVTT() {
-        let vtt = "";
-        await fs.readFile(this.path.split('.txt')[0] + '.srt', 'UTF-8', (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                vtt = this.srt2VTT(data);
-                if (!vtt) return;
-                let f = fs.createWriteStream(this.path.split('.txt')[0] + '.vtt', {flags: 'a+'});
-                f.write(vtt);
-                f.close();
-            }
-        });
-    }
-
-    monitor() {
-        this.live.on('open', () => console.log(this.roomId + 'Connection is established'));
-        this.live.on('DANMU_MSG', (data) => {
-            let danmakuText = data['info'][1];
-            console.log(danmakuText);
-            let s = this.danmakuFilter(danmakuText);
-            if (s) {
-                this.writePlainText(s);
-                if (config.VTT || config.SRT) this.writeSubtitle(s);
-            }
-        });
-    };
-
-    watch() {
-        const _watch = () => {
-            if (currentLive[this.roomId] === 0) {
-                this.emit('close');
-                return
-            } else {
-                console.log(`KeepAlive ${this.roomId}`)
-            }
-            setTimeout(_watch, 3000)
-        };
-        setTimeout(_watch, 3000)
-    };
-
-    danmakuFilter(raw) {
-        let leftPos = raw.indexOf("【");
-        let rightPos = raw.indexOf("】");
-
-        if (leftPos === -1 && rightPos === -1) return null;
-        if (leftPos !== 0) raw = raw.replace("【", "：");
-
-        return raw.replace("【", "").replace("】", "");
+        fs.writeSync(this.jsonFile, JSON.stringify(data, null, 2));
     }
 }
